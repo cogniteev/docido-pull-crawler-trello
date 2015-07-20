4e6a466fbe9853513f1941a60560d54a0b5e00c8
Tristan Carel <tristan@cogniteev.com>
2015-07-20 16:51:15 +0200

    subtask implementation cannot be part of a Component
    There is a serialization issue in Celery when passing an instance function (or static function) of a Component class in `iter_crawl_tasks`.

--- dpc_trello/crawler.py
+++ dpc_trello/crawler.py
@@ -1,6 +1,26 @@
 
+import functools
+
 from docido_sdk.core import Component, implements
 from docido_sdk.crawler import ICrawler
+
+"""
+FIXME: for now it is not possible to pass an instance function
+nor a static function of class TrelloCrawler in iter_crawl_tasks
+parameters because Celery is not able to serialize it
+
+  File "/goinfre/tristan/src/bitbucket/docido-contrib-crawlers/.env/lib/python2.7/site-packages/kombu/serialization.py"
+, line 357, in pickle_dumps
+    return dumper(obj, protocol=pickle_protocol)
+EncodeError: Can't pickle <type 'instancemethod'>: attribute lookup __builtin__.instancemethod failed
+"""
+
+def subtask(index=None, oauth_token=None, foo=None, bar=None):
+    print 'start sub-task'
+    print '  index=%r' % index
+    print '  oauth_token=%r' % oauth_token
+    print '  foo=%r' % foo
+    print '  bar=%r' % bar
 
 class TrelloCrawler(Component):
     implements(ICrawler)
@@ -17,13 +37,8 @@ class TrelloCrawler(Component):
         print '  oauth_token=%r' % oauth_token
         print '  full=%r' % full
         return [
-            functools.partial(self.subtask, foo='fooparam1', bar='barparam1'),
-            functools.partial(self.subtask, foo='fooparam2', bar='barparam2'),
+           (subtask, {'foo':'fooparam1', 'bar':'barparam1'}),
+           (subtask, {'foo':'fooparam2', 'bar':'barparam2'}),
         ]
 
-    def subtask(self, index=None, oauth_token=None, foo=None, bar=None):
-        print 'start sub-task'
-        print '  index=%r' % index
-        print '  oauth_token=%r' % oauth_token
-        print '  foo=%r' % foo
-        print '  bar=%r' % bar
+
