0aa9169e67bebbc878badcc41d1842924ac2a878
tsanch3z <mail.tsanchez@gmail.com>
2015-08-26 11:01:06 +0200

    Update dpc_trello
    * Add logger support
    * Use docido_sdk's OauthToken
    * Remove use of bugged crads's create_date property

--- dpc_trello/crawler.py
+++ dpc_trello/crawler.py
@@ -5,6 +5,8 @@ from docido_sdk.core import Component, implements
 from docido_sdk.crawler import ICrawler
 
 from trello import TrelloClient
+from dateutil import parser
+import time
 
 """
 FIXME: for now it is not possible to pass an instance function
@@ -20,27 +22,38 @@ EncodeError: Can't pickle <type 'instancemethod'>: attribute lookup __builtin__.
 
 def _create_trello_client(oauth_token):
     return TrelloClient(
-        api_key='AN_API_KEY',
-        api_secret='AN_API_SECRET',
-        token=oauth_token
+        api_key=token.consumer_key,
+        api_secret=token.token_secret,
+        token=token.access_token
     )
 
 
-def handle_board_cards(board_id, index=None, oauth_token=None):
-    trello = _create_trello_client(oauth_token)
+def handle_board_cards(board_id, push_api=None, token=None, logger=None):
+    trello = _create_trello_client(token)
     board = trello.get_board(board_id)
+    docido_cards = []
     for card in board.get_cards():
+        card.fetch_actions('createCard,copyCard,convertToCardFromCheckItem')
+        if not any(card.actions):
+            # no card creation event, author cannot get inferred
+            continue
+        date = parser.parse(card.actions[0]['date'])
+        author = card.actions[0]['memberCreator']
         docido_card = {
+            'attachments': [],
             'id': card.id,
             'title': card.name,
             'description': card.description,
+            'date': time.mktime(date.utctimetuple()) * 1e3
+            + date.microsecond / 1e3,
             'author': {
                 # TODO: find a way to retrieve that information
             },
-            'kind': 'activity'
+            'kind': 'note'
         }
-        import json
-        print json.dumps(docido_card, indent=1)
+        docido_cards.append(docido_card)
+    push_api.push_cards(docido_cards)
+    # print len(list(push_api.search_cards(None)))
 
 
 class TrelloCrawler(Component):
@@ -49,14 +62,15 @@ class TrelloCrawler(Component):
     def get_service_name(self):
         return 'trello'
 
+    def get_account_login(self, token):
+        return 'foo'
+
     def iter_crawl_tasks(self, index, token, logger, full=False):
         trello = _create_trello_client(token)
         return [
             functools.partial(
                 handle_board_cards,
-                board.id,
-                index=index,
-                oauth_token=oauth_token
+                board.id
             )
             for board in trello.list_boards()
         ][:1]
